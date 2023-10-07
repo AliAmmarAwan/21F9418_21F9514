cbe278234f4d20ad4323b84380b3d2478ab00096
“[firstname <[f219418@cfd.nu.edu.pk]>
2023-10-07 22:04:44 +0500

    I added multiply and division function

--- Calculator.java
+++ Calculator.java
@@ -9,14 +9,25 @@ public class Calculator {
         int subtract = a - b;
         return subtract;
     }
+    public static int divide(int a, int b) {
+        int div = a / b;
+        return div;
+    }
+    public static int multiply(int a, int b) {
+        int mul= a * b;
+        return mul;
+    }
+    
 
     public static void main(String[] args) {
        
         int result = add(5, 3);
         int result1 = sub(5, 3);
-
-      
+        int result2 = divide(5, 3);
+        int result3 = multiply(5, 3);
         System.out.println("The sum is: " + result);
         System.out.println("The subtract is: " + result1);
+        System.out.println("The division is: " + result2);
+        System.out.println("The multiply is: " + result3);
     }
 }
